35baf8124c3deded3c8cd263efaf0c538843414b
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-23 22:43:36 -0500

    el operador spread

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ECMAS6</title>
+	<script src="js/js.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,19 @@
+
+
+function agregar_alumnos( array_alumnos, ...nuevosAlumnos ) {
+
+	console.log(nuevosAlumnos);
+
+	for (let i = 0; i < nuevosAlumnos.length; i++) {
+		array_alumnos.push( nuevosAlumnos[i] );
+	}
+
+	return array_alumnos;
+
+}
+
+
+let array_alumnos = ['Ramón'];
+let array_alumnos_seguimiento = agregar_alumnos(array_alumnos, 'Diana', 'Ruben', 'Juan');
+
+console.log( array_alumnos_seguimiento );
