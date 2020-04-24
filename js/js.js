c4e4c9a52fa7b5526057b9d54aa0181c1f308999
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-23 23:32:06 -0500

    parametro rest

--- js/js.js
+++ js/js.js
@@ -1,23 +1,8 @@
 
 
-function agregar_alumnos( array_alumnos, ...nuevosAlumnos ) {
-
-	console.log(nuevosAlumnos);
-
-	for (let i = 0; i < nuevosAlumnos.length; i++) {
-		array_alumnos.push( nuevosAlumnos[i] );
-	}
-
-	return array_alumnos;
-
+function agregar_alumnos( ...array_alumnos, ...nuevosAlumnos ) {
+	//cualquier cosa
 }
-
-
-let array_alumnos = ['Ramón'];
-let array_alumnos_seguimiento = agregar_alumnos(array_alumnos, 'Diana', 'Ruben', 'Juan');
-
-console.log( array_alumnos_seguimiento );
-
 
 
 
