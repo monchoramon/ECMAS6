73e846698d2ea0b94867f29e43a5f07862422b06
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-27 22:46:37 -0500

    Romper la relación de referencia de los objetos

--- js/js.js
+++ js/js.js
@@ -1,9 +1,26 @@
-//ECMAS 5
-var numeros = [1, 5, 43, 133, 4234, 4557];
-var valorMaximoNumeros = Math.max.apply( Math, numeros);
-console.log(valorMaximoNumeros);
+let persona1 = {
+    nombre: 'Ramón',
+    edad: 100
+};
 
-//ECMAS 6
-var numeros = [1, 5, 43, 133, 4234, 4557];
-var valorMaximoNumeros = Math.max(...numeros);
-console.log(valorMaximoNumeros);	
+/*
+	En JS todos los objetos son pasados por referencia, lo que hace
+	que el cambio de los objetos no sea una asignación por valor, si no
+	que hace una asignacíón en memoria donde se encuentra el obejeto,
+	por lo tanto el valor se cambia implicitamente no importa que objeto
+	se utilice, siempre los cambios se haran con los mismos datos 
+	a los objetos.
+*/
+
+//Encerrando entre llaves y utilizando el operador spread, 
+//podemos copiar todas las propiedades del cualquier objeto
+//rompera la relación entre objetos, serán independientes.
+
+let persona2 = persona1;
+persona2.edad = 120;
+
+let persona3 = { ...persona1 }
+persona3.nombre = 'Daniel';
+persona3.edad   = 130;
+
+console.log(persona1, persona2, persona3);
