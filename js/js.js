b4dc36282e847df86a4ce6b4879f001ca042a1c2
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-27 22:58:50 -0500

    pasando propiedades entre objetos y conservando otras, utilizando el operador SPREAD

--- js/js.js
+++ js/js.js
@@ -1,26 +1,23 @@
 let persona1 = {
-    nombre: 'Ramón',
-    edad: 100
+    nombre: 'David',
+    edad: 33
 };
 
-/*
-	En JS todos los objetos son pasados por referencia, lo que hace
-	que el cambio de los objetos no sea una asignación por valor, si no
-	que hace una asignacíón en memoria donde se encuentra el obejeto,
-	por lo tanto el valor se cambia implicitamente no importa que objeto
-	se utilice, siempre los cambios se haran con los mismos datos 
-	a los objetos.
-*/
+let persona2 = {
+    nombre: 'Ramón',
+    edad: 100,
+    conduce: true,
+    tiene_vehiculo: true,
+    vegetariano: false,
+    casado: true
+};
 
-//Encerrando entre llaves y utilizando el operador spread, 
-//podemos copiar todas las propiedades del cualquier objeto
-//rompera la relación entre objetos, serán independientes.
+persona1 = {
+    //padamos todo lo que tiene el objeto, persona2
+    ...persona2,
+    //conservando lo que tiene el objeto, persona1
+    ...persona1
+};
 
-let persona2 = persona1;
-persona2.edad = 120;
 
-let persona3 = { ...persona1 }
-persona3.nombre = 'Daniel';
-persona3.edad   = 130;
-
-console.log(persona1, persona2, persona3);
+console.log(persona1, persona2);
