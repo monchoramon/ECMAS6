67c4b2c5d385536c036c93d479aa1b2bf7966e4d
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-29 00:26:30 -0500

    arrow functions

--- js/js.js
+++ js/js.js
@@ -1,18 +1,1 @@
-//por separado
-function operadorRest(saludo, ...personas) {
-    for (i in personas) {
-        console.log(`${saludo} ${personas[i]}`);
-    }
-}
-
-//Propagacion
-function operadorSpread(saludo, ...personas) {
-    console.log(`${saludo} ${personas}`);
-}
-
-operadorRest('Hola', 'Marcos', 'Daniel', 'Juan');
-
-let personas      = ['daniel', 'sandra', 'gonzalo', 'maria'];
-let otrasPersonas = [...personas, 'ramon', 'juan'];
-operadorSpread('Que tal!!', otrasPersonas);
-
+//Funciones de Flecha ó Arrow Functions
