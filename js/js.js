d6f2e81de852c20087a460be99d876278089faff
momonchoramon <teamcygnus1@solei-mx.com>
2020-04-30 20:45:09 -0500

    trabajando con objetos

--- js/js.js
+++ js/js.js
@@ -20,3 +20,21 @@ let numerosPrimero = [0];
 let numeros = [...numerosPrimero,1,2,3,4,5,6,7,8,9];
 
 console.log( sumar2(numeros) );
+
+//Retornando y/o manejo de objeto
+
+var objeto1 = function(id, nombre){
+	return {
+		id:id,
+		nombre:nombre
+	}
+}
+
+var objeto2 = (id, nombre) => ({
+	id:id,
+	nombre:nombre
+});
+
+
+console.log( objeto1(1,'Ramón Martínez Cruz') );
+console.log( objeto2(2,'Daniel Gomez') );
