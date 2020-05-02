ee93b85c474a154b0d7d57afd090b72329099f03
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-02 16:23:47 -0500

    extensiones de objetos literales

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ Objetos literales en ES6, este tipo de estructura es el más utilizados
 en JS, (JSON se construyó por medio de esta sintaxis)
 */
 
-function crearPersona( nombre, apellidos, edad){
+function crearPersona( nombre, apellidos, edad) {
 	return {
 		nombre,
 		apellidos,
@@ -13,3 +13,11 @@ function crearPersona( nombre, apellidos, edad){
 
 var ramon = crearPersona("Ramón", "Martínez Cruz", 26);
 console.log( ramon );
+
+//Métodos Concisos
+
+
+
+
+
+
