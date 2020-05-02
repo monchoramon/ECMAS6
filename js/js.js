85bdb9b77829b0fbc1b4aec2bbf5676add255a78
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-02 16:40:26 -0500

    metodos concisos

--- js/js.js
+++ js/js.js
@@ -14,9 +14,17 @@ function crearPersona( nombre, apellidos, edad) {
 var ramon = crearPersona("Ramón", "Martínez Cruz", 26);
 console.log( ramon );
 
-//Métodos Concisos
+//Métodos Concisos, en ES6 ya no es necesario poner la palabra resevada function cuando se
+//crea un metodo o función, solo basta con poner el nombre de la función.
 
+var persona = {
+	nombre: "Ramón",
+	getNombre() {
+		console.log(this.nombre);
+	}
+}
 
+persona.getNombre();
 
 
 
