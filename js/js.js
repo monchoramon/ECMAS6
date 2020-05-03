881eacd4be590804a258f24c1121d259062e4575
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-02 19:25:06 -0500

    Adiciones a los objetos

--- js/js.js
+++ js/js.js
@@ -65,3 +65,30 @@ var persona = {
 };
 
 console.log( persona );
+
+//Nuevos metodos:Object.is()
+
+console.log(+0==-0);
+console.log(+0===-0);
+console.log( Object.is(+0, -0) );
+
+console.log("===");
+
+console.log(NaN==NaN);
+console.log(NaN===NaN);
+console.log(Object.is(NaN,NaN));
+
+console.log("===");
+
+console.log(5==5);
+console.log(5=="5");
+
+console.log("===");
+
+console.log(5===5);
+console.log(5==="5");
+
+console.log("==== 5 contra 5 ====");
+
+console.log(Object.is(5, 5));
+console.log(Object.is(5, "5"));
