cbb7b5f9f4b908c8f498550638350c9892b7fd7f
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-07 15:58:48 -0500

    simbolos y propiedades

---
+++ js/js.js
@@ -0,0 +1,29 @@
+
+/*let primerNombre    = "primerNombre";    
+let primerApellido  = "primerApellido";  
+let segundoApellido = "segundoApellido";*/
+
+let primerNombre    = Symbol("primer nombre");  
+let primerApellido  = Symbol("primer apellido");  
+let segundoApellido = Symbol("segundo apellido");
+
+let personaNombre = {};
+
+personaNombre[primerNombre] = "Ramón";
+
+let personaApellidos = {
+	[primerApellido]: "Martínez"
+};
+
+personaApellidos[segundoApellido] = "Cruz";
+
+console.log( personaNombre);
+console.log( personaNombre[primerNombre]);
+
+console.log( personaApellidos );
+console.log ( 
+			  	personaApellidos[primerApellido], 
+	          	personaApellidos[segundoApellido] 
+	        );
+
+console.log( typeof primerNombre );
