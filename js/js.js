e4af497aae181508130f0121fa7ac9322466324c
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-10 23:29:03 -0500

    Introducción a los Mapas

---
+++ js/js.js
@@ -0,0 +1,19 @@
+/*
+Mapas, es un nuevo tipo de colección de datos, que por mucho tiempo
+fue necesario en JS.
+*/
+
+//Ej. 
+/*
+	Llave ------------> Valor
+	nombre				Ramón
+	edad                26
+
+	Los mapas al igual que los Strong SET tienen:
+	1. Has()
+	2. delete()
+	3. clear()
+	4. size()
+	5. iteraciones
+*/
+
