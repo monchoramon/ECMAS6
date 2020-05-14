b7c1cf8a54e88b02de4dd1a60c68e78f667ede2c
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-13 22:48:11 -0500

    inicializaciones de los maps

--- js/js.js
+++ js/js.js
@@ -10,10 +10,37 @@ fue necesario en JS.
 	edad                26
 
 	Los mapas al igual que los Strong SET tienen:
-	1. Has()
-	2. delete()
-	3. clear()
+	1. has() //verifica si el key pasado esta definido.
+	2. delete() //eliminar un mapa por medio del key pasado.
+	3. clear() //borra todo los mapas, pasando como parametro el mapa que se quiera borrar.
 	4. size()
 	5. iteraciones
 */
 
+var mapa = new Map();
+
+mapa.set("nombre", "Ramón");
+mapa.set("edad", 26);
+mapa.set("apellido");
+mapa.set({saludo1:'Hola 1'},{saludo2:'Hola 2'});
+
+console.log( mapa );
+console.log( mapa.size );
+
+console.log( mapa.get("nombre") );
+console.log( mapa.get("edad") );
+console.log( mapa.has("nombre") );
+console.log( mapa.has("apellido") );
+
+mapa.delete("nombre");
+console.log( mapa.get("nombre") );
+console.log( mapa.has("nombre") );
+
+mapa.set("edad");
+console.log( mapa );
+
+//Inicializaciones de los mapas
+
+var mapa = new Map( [ ["nombre","Ramón"], [undefined, function(fecha){ alert(`${fecha}`); }(new Date())] ] );
+console.log( mapa );
+console.log( mapa.get(undefined) );
