a19b34dcd5b75dfeb20721fe9c749f3ba9a3aa37
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-18 21:52:42 -0500

    Introdución a las clases

--- js/js.js
+++ js/js.js
@@ -1,1 +1,19 @@
 //Introduccion a las clases en JS
+
+function Persona ( nombre ) {
+	this.nombre = nombre;
+
+	this.gritarNombre = function() {
+		console.log( this.nombre.toUpperCase() );
+	}
+}
+
+Persona.prototype.decirNombre = function() {
+	console.log( this.nombre );
+}
+
+console.log( Persona.prototype );
+
+var persona = new Persona("Ramon");
+persona.decirNombre();
+persona.gritarNombre();
