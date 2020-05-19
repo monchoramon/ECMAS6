c8c0a414c8cc258224c4a388a506d92ce50b0962
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-18 22:43:30 -0500

    Declaración básica de una clase en ES6

--- js/js.js
+++ js/js.js
@@ -17,3 +17,25 @@ var persona = Persona("Ramon");
 //persona.gritarNombre();
 
 console.log( persona instanceof Persona  );
+console.log( persona instanceof Object  );
+
+//Declaración básica de una clase en ES6
+class Persona2 {
+	
+	constructor( nombre ) {
+		this.nombre = nombre;
+	}	
+
+	decirNombre() {
+		console.log(this.nombre);
+	}
+
+}
+
+var persona2 = new Persona2("Juan");
+persona2.decirNombre();
+
+console.log( persona2 instanceof Persona2  );
+console.log( persona2 instanceof Object  );
+console.log( typeof Persona2  );
+console.log( typeof Persona2.prototype  );
