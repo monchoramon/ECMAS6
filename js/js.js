82fd7d700c4e33b47a4c26e8a1de0290fcfbf908
momonchoramon <teamcygnus1@solei-mx.com>
2020-05-19 23:29:32 -0500

    Profundizando en las clases

--- js/js.js
+++ js/js.js
@@ -1,2 +1,28 @@
 //Profundizando en las clases
 
+//Los metodos computados son nombres los cuales se pueden 
+//defini por medio de una variable.
+let nombreMetodo = "gritarNombre";
+
+class Persona {
+	
+	constructor(nombre){
+		this.nombre = nombre;
+	}
+
+	decirNombre(){
+		console.log( this.nombre );
+	}
+
+	[nombreMetodo]() {
+		console.log(this.nombre.toUpperCase() );
+	}
+
+	static crear( nombre ) {
+		return new Persona(nombre);
+	}
+
+}
+
+let nombre = Persona.crear("Ramón");
+console.log( nombre );
